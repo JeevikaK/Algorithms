4034ff5f1c421b3331bc050285e62965b62d49fd
Jeevika K <jeevika.kiran@gmail.com>
2022-06-21 20:53:50 +0530

    Update knapsack_algo.c

--- knapsack_algo.c
+++ knapsack_algo.c
@@ -1,100 +1,66 @@
-#include<stdio.h>
-#include<stdlib.h>
-int w[10],p[10],v[10][10];
-int n,m;
-int max(int a,int b) //function to find max element
-{
-if(a>b)
-{
-return a;
-}
-else
-{
-return b;
-}
+#include <stdio.h>
+int i,j,m,n,p[10],w[10],v[10][10];
+
+int max(int a, int b){
+    if(a>b)
+        return a;
+    return b;
 }
 
-void knapsack()
-{
-for(int i=0;i<=n;i++)
-{
-for(int j=0;j<=m;j++)
-{
-
-/*if no of objects or capacity of knapsack is 0, then profit is also 0*/
-if(i==0&&j==0)
-{
-v[i][j]=0;
+void knapsack(){
+    for(i=0;i<=n;i++){
+        for(j=0;j<=m;j++){
+            if(i==0 || j==0)
+                v[i][j]=0;
+            else if(w[i]>j)
+                v[i][j]=v[i-1][j];
+            else   
+                v[i][j]=max(v[i-1][j], v[i-1][j-w[i]]+p[i]);
+        }
+    }
+    printf("The optimal solution is %d\n", v[n][m]);
+    printf("The Knapsack table is as follows \n");
+    for(i=0;i<=n;i++){
+        for(j=0;j<=m;j++){
+            printf("%d\t", v[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-/*weight of object i is greater than remaining capacity of knapsack*/
-else if(w[i]>j)
-{
-v[i][j]=v[i-1][j]; //i th object is rejected
+void element(){
+    int x[n];
+    for(i=1;i<=n;i++)
+        x[i] = 0;
+    
+    int i=n;
+    int j=m;
+    while(i>0 && j>0){
+        // printf("%d %d\n", v[i][j], v[i-1][j]);
+        if(v[i][j]!=v[i-1][j]){
+            printf("i=%d\n", i);
+            x[i]=1;
+            j=j-w[i];
+        }
+        i--;
+    }
+    for(i=1;i<=n;i++)
+        printf("%d\t", x[i]);
+    printf("\n");
 }
 
-/*weight of object i is lesser than remaining capacity of knapsack*/
-else
-{
-v[i][j]=max(v[i-1][j],v[i-1][j-w[i]]+p[i]); //formula
-}
-}
-}
-
-printf("the optimum solution is %d\n",v[n][m]);
-printf("tabular column is:\n"); //shows tracing of the knapsack problem
-for(int i=0;i<n;i++)
-{
-for(int j=0;j<=m;j++)
-{
-printf("%d\t",v[i][j]);
-}
-printf("\n");
-}
-}
-
-void element() //function to find optimum solution using formula
-{
-int x[n];
-for(int i=0;i<n;i++)
-{
-x[i]=0;
-}
-int i=n;
-int j=m;
-while(i>0&&j>0)
-{
-if(v[i][j]!=v[i-1][j])
-{
-x[i]=1;
-j=j-w[i];
-}
-i--;
-}
-
-for(int i=0;i<n;i++)
-{
-printf("%d\t",x[i]);
-}
-}
-
-void main()
-{
-printf("Enter the capacity of the knapsack:\t");
-scanf("%d",&m);
-printf("Enter the number of elements:\t");
-scanf("%d",&n);
-printf("Enter the weights of the elements:\n");
-for(int i=0;i<n;i++)
-{
-scanf("%d",&w[i]);
-}
-printf("Enter the profits of each element:\n");
-for(int i=0;i<n;i++)
-{
-scanf("%d",&p[i]);
-}
-knapsack();
-printf("\n");
-element();
+void main(){
+    printf("Enter the capacity of knapsack\t");
+    scanf("%d", &m); 
+    printf("\nEnter the number of elements\t");
+    scanf("%d", &n);
+    printf("\nEnter the weights of the elements \t");
+    for(i=1;i<=n;i++)
+        scanf("%d", &w[i]);
+    printf("\nEnter the profits of the elements \t");
+    for(i=1;i<=n;i++)
+        scanf("%d", &p[i]);
+    knapsack();
+    printf("\n");
+    element();
 }
